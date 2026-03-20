67b55f4a18532c6be08df5d61188c4b2ad1b6abe
Anindita Sanjagiri <anindita.sanjagiri@gmail.com>
2026-03-20 11:38:13 +0530

    Time: 8 ms (53.34%) | Memory: 19.4 MB (45.53%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,13 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
+        x_str=str(x)
+        return x_str==x_str[::-1]
+
+
         
-        reversed_x = 0
         
-        while x > reversed_x:
-            reversed_x = reversed_x * 10 + x % 10
-            x //= 10
-        
-        return x == reversed_x or x == reversed_x // 10
         
